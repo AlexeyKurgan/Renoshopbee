ac084db0b481856b8f55826c74f3866505f00a06
Alexey Kurgan <alexkurgan19927@gmail.com>
2020-10-14 15:06:16 +0300

    added properties in slick slider and changed slide image

--- src/js/slickSlider.js
+++ src/js/slickSlider.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
     $('.slider').slick({
+        speed: 1200,
         arrows:true,
         dots:true,
+        autoplay:true,
         fade:true,
-        autoplaySpeed:3500,
+        autoplaySpeed:5500,
+        infinite:true,
         centerPadding:'50px',
         prevArrow: '<button id="prev" type="button" class="btn prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
         nextArrow: '<button id="next" type="button" class="btn next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
