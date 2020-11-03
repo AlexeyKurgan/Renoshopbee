0c22001999e595a562bc360856e49c38de3ef2f3
AlexKurgan1992 <Redlix1992777@gmail.com>
2020-11-04 01:05:44 +0300

    getting started with the page shop-products-name

--- src/js/slickSlider.js
+++ src/js/slickSlider.js
@@ -11,4 +11,25 @@ $(document).ready(function(){
         prevArrow: '<button id="prev" type="button" class="btn prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
         nextArrow: '<button id="next" type="button" class="btn next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
     });
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        asNavFor: '.slider-nav',
+        // infinite:true,
+        centerPadding:'50px',
+        autoplaySpeed:1500,
+        autoplay:true,
+        speed: 1200,
+        prevArrow: '<button id="prev" type="button" class="btn prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button id="next" type="button" class="btn next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        centerMode: true,
+        focusOnSelect: true,
+      });
 });
