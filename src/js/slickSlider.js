6a26ed8887e13b71753736d194647da8919478df
AlexKurgan1992 <Redlix1992777@gmail.com>
2020-11-08 23:12:28 +0300

    added styles for product card

--- src/js/slickSlider.js
+++ src/js/slickSlider.js
@@ -15,21 +15,23 @@ $(document).ready(function(){
     $('.slider-for').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: true,
+        arrows:false,
         asNavFor: '.slider-nav',
         // infinite:true,
         centerPadding:'50px',
         autoplaySpeed:1500,
-        autoplay:true,
-        speed: 1200,
-        prevArrow: '<button id="prev" type="button" class="btn prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
-        nextArrow: '<button id="next" type="button" class="btn next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+        // autoplay:true,
+        // speed: 1200,
+       
       });
+
       $('.slider-nav').slick({
+        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         asNavFor: '.slider-for',
-        centerMode: true,
-        focusOnSelect: true,
+        arrows: true,
+        prevArrow: '<button id="prev" type="button" class="prev-arrow"><i class="fas fa-angle-double-left"></i></button>',
+        nextArrow: '<button id="next" type="button" class="next-arrow"><i class="fas fa-angle-double-right"></i></button>'
       });
 });
